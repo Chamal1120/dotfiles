f39583885d931e376151cd8fa3c0fb75d029d4bf
Chamal1120RE <randy99.foss@gmail.com>
2024-08-14 04:13:53 +0530

    add new test pattern matching

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -13,6 +13,9 @@ return {
     "nvim-telescope/telescope-ui-select.nvim",
     config = function()
       require("telescope").setup({
+        defaults = {
+          file_ignore_patterns = { "node_modules", ".git/", "large_directory/" },
+        },
         extensions = {
           ["ui-select"] = {
             require("telescope.themes").get_dropdown({}),
