554303063be377c13e353549442d525e13f0c94a
Chamal1120 <kenway213beep@gmail.com>
2024-10-02 11:43:15 +0530

    remove bashls from mason setup

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -13,7 +13,7 @@ return {
     },
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "tsserver", "bashls", "ruff_lsp", "tailwindcss" },
+        ensure_installed = { "lua_ls", "tsserver", "ruff_lsp", "tailwindcss" },
       })
     end,
   },
