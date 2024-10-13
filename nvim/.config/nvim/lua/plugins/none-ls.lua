a1d917c555577ec021c3611f4285d47201a873f1
Chamal1120 <kenway213beep@gmail.com>
2024-10-13 19:33:32 +0530

    remove isort and blade formatter

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -17,10 +17,8 @@ return {
 				}),
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.black,
-        null_ls.builtins.formatting.isort,
 				null_ls.builtins.formatting.prettier,
 				null_ls.builtins.formatting.clang_format,
-				null_ls.builtins.formatting.blade_formatter,
 			},
 		})
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
