af0a0e9ffc9ff20c5163419d63e7b78c9b9d17d2
Chamal1120 <kenway213beep@gmail.com>
2024-12-10 11:30:18 +0530

    exclude non-lang filetypes and bufs

--- nvim/.config/nvim/lua/plugins/indent-lines.lua
+++ nvim/.config/nvim/lua/plugins/indent-lines.lua
@@ -1,10 +1,15 @@
 return {
 	"lukas-reineke/indent-blankline.nvim",
 	main = "ibl",
-	---@module "ibl"
-	---@type ibl.config
-	opts = {},
-  config = function()
-    require("ibl").setup()
-  end,
+  --@module "ibl"
+  --@type ibl.config
+	opts = {
+		exclude = {
+			filetypes = { "dashboard", "alpha", "startify", "NvimTree", "help" },
+			buftypes = { "nofile", "terminal" },
+		},
+	},
+	config = function(_, opts)
+		require("ibl").setup(opts)
+	end,
 }
