9d80505566bf323f57d896f541d584d76d329b1f
Chamal1120 <kenway213beep@gmail.com>
2024-12-26 18:44:59 +0530

    change nvim dashboard UI

--- nvim/.config/nvim/lua/plugins/dashboard.lua
+++ nvim/.config/nvim/lua/plugins/dashboard.lua
@@ -1,38 +1,49 @@
 return {
-	"nvimdev/dashboard-nvim",
-	event = "VimEnter",
-	config = function()
-		require("dashboard").setup({
-			theme = "hyper",
-			config = {
-				week_header = {
-					enable = true,
-				},
-				shortcut = {
-					{ desc = "󰊳 Update", group = "@property", action = "Lazy update", key = "u" },
-					{
-						icon = " ",
-						icon_hl = "@variable",
-						desc = "Files",
-						group = "Label",
-						action = "Telescope find_files",
-						key = "f",
-					},
-					{
-						desc = " Apps",
-						group = "DiagnosticHint",
-						action = "Telescope app",
-						key = "a",
-					},
-					{
-						desc = " dotfiles",
-						group = "Number",
-						action = "Telescope dotfiles",
-						key = "d",
-					},
-				},
-			},
-		})
-	end,
-	dependencies = { { "nvim-tree/nvim-web-devicons" } },
+  "nvimdev/dashboard-nvim",
+  event = "VimEnter",
+  config = function()
+    require("dashboard").setup({
+      theme = "hyper",
+      config = {
+        week_header = {
+          enable = true,
+        },
+        shortcut = {
+          { desc = "󰊳 Update", group = "@property", action = "Lazy update", key = "u" },
+          {
+            icon = " ",
+            icon_hl = "@variable",
+            desc = "Files",
+            group = "Label",
+            action = "Telescope find_files",
+            key = "f",
+          },
+          {
+            desc = " Apps",
+            group = "DiagnosticHint",
+            action = "Telescope app",
+            key = "a",
+          },
+          {
+            desc = " dotfiles",
+            group = "Number",
+            action = "Telescope dotfiles",
+            key = "d",
+          },
+        },
+        project = {
+          enable = true,
+          limit = 4,
+          label = "Can't think of? Continue one from here,",
+          action = "Telescope find_files cwd=",
+        },
+        mru = {
+          enable = false,
+          cwd_only = false,
+        },
+        footer = {"Hello Chamal 👋, Let's Code!"},
+      },
+    })
+  end,
+  dependencies = { { "nvim-tree/nvim-web-devicons" } },
 }
