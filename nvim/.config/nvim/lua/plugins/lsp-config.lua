af5e7326f04cf14cb3ba0826c8ec1f0a5220a15e
Chamal1120 <kenway213beep@gmail.com>
2025-01-05 17:59:37 +0530

    replace ruff with pylint and pylsp and add cppcheck

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -29,28 +29,34 @@ return {
 		lazy = false,
 		opts = {
 			servers = {
+        html = {},
+        cssls = {},
+        jinja_lsp = {},
 				lua_ls = {},
 				ts_ls = {},
 				bashls = {},
 				clangd = {},
 				tailwindcss = {},
 				rust_analyzer = {},
-				ruff = {
-					cmd = { vim.loop.cwd() .. "/.venv/bin/ruff-lsp" }, -- Use ruff-lsp from local .venv
-					trace = "messages",
-					init_options = {
-						settings = {
-							loglevel = "debug",
-							args = { "--line-length=88" },
+				pylsp = {
+					settings = {
+						pylsp = {
+							configurationSources = { "flake8" },
 						},
 					},
+					on_new_config = function(config, root_dir)
+						local venv_path = root_dir .. "/.venv/bin/python"
+						if vim.fn.filereadable(venv_path) == 1 then
+							config.cmd = { venv_path, "-m", "pylsp" }
+						end
+					end,
+					root_dir = require("lspconfig.util").root_pattern(".venv", ".git"),
 				},
 			},
 		},
 		config = function(_, opts)
 			local lspconfig = require("lspconfig")
 			local blink_cmp = require("blink.cmp")
-			
 			for server, config in pairs(opts.servers) do
 				config.capabilities = blink_cmp.get_lsp_capabilities(config.capabilities)
 				lspconfig[server].setup(config)
--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -12,6 +12,17 @@ return {
 				require("none-ls.diagnostics.eslint_d").with({
 					command = "node_modules/.bin/eslint_d",
 				}),
+				null_ls.builtins.diagnostics.pylint.with({
+					command = (function()
+						local venv_pylint = vim.fn.getcwd() .. "/.venv/bin/pylint"
+						if vim.fn.executable(venv_pylint) == 1 then
+							return venv_pylint
+						else
+							return "pylint"
+						end
+					end)(),
+				}),
+				null_ls.builtins.diagnostics.cppcheck,
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.black,
 				null_ls.builtins.formatting.prettier.with({
