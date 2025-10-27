90b14e20d76892e3056500858977487653661fdf
Chamal1120 <kenway213beep@gmail.com>
2025-10-27 17:45:40 +0530

    Update nvim config, gtk and foot

--- configs/nvim/.config/nvim/lua/lsp/init.lua
+++ configs/nvim/.config/nvim/lua/lsp/init.lua
@@ -1,39 +1,43 @@
 -- Configure LSP clients
 
 -- Set default root markers for all clients
-vim.lsp.config('*', {
-  root_markers = { '.git' },
+vim.lsp.config("*", {
+  root_markers = { ".git" },
 })
 
 -- List of LSPs
 local servers = {
   ts_ls = {
-    cmd = { 'typescript-language-server', '--stdio' },
-    filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
+    cmd = { "typescript-language-server", "--stdio" },
+    filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
   },
   lua_ls = {
-    cmd = { 'lua-language-server' },
-    filetypes = { 'lua' },
+    cmd = { "lua-language-server" },
+    filetypes = { "lua" },
     settings = {
       Lua = {
-        runtime = { version = 'LuaJIT' },
-        diagnostics = { globals = { 'vim' } },
+        runtime = { version = "LuaJIT" },
+        diagnostics = { globals = { "vim" } },
         workspace = { library = vim.api.nvim_get_runtime_file("", true) },
         telemetry = { enable = false },
       },
     },
   },
   html = {
-    cmd = { 'vscode-html-language-server', '--stdio' },
-    filetypes = { 'html' },
+    cmd = { "vscode-html-language-server", "--stdio" },
+    filetypes = { "html" },
   },
   tailwindcss = {
-    cmd = {'tailwindcss-language-server', '--stdio'},
-    filetypes = { 'html', 'razor', 'tsx', 'jsx' },
+    cmd = { "tailwindcss-language-server", "--stdio" },
+    filetypes = { "html", "razor", "tsx", "jsx" },
   },
   rust_analyzer = {
-    cmd = { 'rust-analyzer' },
-    filetypes = { 'rust' },
+    cmd = { "rust-analyzer" },
+    filetypes = { "rust" },
+  },
+  tinymist = {
+    cmd = { "tinymist" },
+    filetypes = { "typst" },
   },
   jtdls = {},
 }
@@ -45,7 +49,7 @@ for name, config in pairs(servers) do
 end
 
 -- Setup buffer-local LSP keymaps on attach
-vim.api.nvim_create_autocmd('LspAttach', {
+vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(args)
     local bufnr = args.buf
 
@@ -56,22 +60,22 @@ vim.api.nvim_create_autocmd('LspAttach', {
     local opts = { noremap = true, silent = true, buffer = bufnr }
 
     -- Go to definition
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
     -- Hover documentation
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
     -- Rename symbol
-    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
     -- Code actions
-    vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
     -- Show references
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
     -- Implementation
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
     -- Type definition
-    vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, opts)
     -- Signature help
-    vim.keymap.set('i', '<C-s>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set("i", "<C-s>", vim.lsp.buf.signature_help, opts)
     -- Diagnostics
-    vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
+    vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
   end,
 })
